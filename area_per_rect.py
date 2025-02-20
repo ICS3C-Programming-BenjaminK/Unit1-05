b9a43cab0111f9d42a51180e0a8f4bd1f073a49c
Benjamin Kamanda <benjamin.kamanda@stu.ocsb.ca>
2025-02-20 17:20:34 +0000

    Made program to calculate area of rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,10 @@
+def main():
+    # area
+    print("if a rectangle has the dimentions:")
+    print("5cm *2cm")
+    print("")
+    print("Area is {}cm^2".format(5 * 3))
+
+
+if __name__ == "__main__":
+    main()
